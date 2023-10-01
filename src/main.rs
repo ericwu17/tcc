8aefc15a6c0a70327c9340545e8949180cea1bba
Eric <eric.dianhao.wu@gmail.com>
2023-10-01 21:43:11 +0000

    add clap

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::io::Write;
 
 use std::process::Command;
 
+use clap::Parser;
 use codegen::generate_code;
 use parser::generate_program_ast;
 use tokenizer::get_tokens;
@@ -16,26 +17,40 @@ const ASM_FILE_NAME: &str = "out.asm";
 const OBJ_FILE_NAME: &str = "out.o";
 const EXEC_FILE_NAME: &str = "a.out";
 
-fn main() {
-    let input_filepath = std::env::args().nth(1).expect("Please give a filename");
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Name of input file containing C source code
+    filename: String,
+    /// Skip the assembly and link step of compilation, only generating the assembly file
+    #[arg(short = 'n', long = "no-assemble")]
+    no_assemble: bool,
+}
 
-    let mut input_file =
-        File::open(&input_filepath).expect(&format!("could not open file: {}", &input_filepath));
+fn main() {
+    let cli = Cli::parse();
+
+    let input_filepath = cli.filename;
+    let no_assemble = cli.no_assemble;
+
     let mut contents = String::new();
-    input_file
+    File::open(&input_filepath)
+        .expect(&format!("could not open file: {}", &input_filepath))
         .read_to_string(&mut contents)
         .expect(&format!("error reading file: {}", &input_filepath));
 
     let tokens = get_tokens(contents);
-
     let program_AST = generate_program_ast(&mut tokens.into_iter().peekable());
-
     let asm_code: String = generate_code(program_AST);
 
-    let mut file = File::create(ASM_FILE_NAME).unwrap();
-    file.write(asm_code.as_bytes()).unwrap();
+    File::create(ASM_FILE_NAME)
+        .expect("error creating ASM output file.")
+        .write(asm_code.as_bytes())
+        .expect("error writing output to ASM output file.");
 
-    assemble_and_link();
+    if !no_assemble {
+        assemble_and_link();
+    }
 }
 
 fn assemble_and_link() {
