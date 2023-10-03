5d80a36c33bc1fbdbc6aaf947e2aef49a98a7e63
Eric <eric.dianhao.wu@gmail.com>
2023-10-03 03:52:10 +0000

    add integration tests

---
+++ tests/programs/operators/test_addition.c
@@ -0,0 +1,4 @@
+int main() {
+  // return the bitwise complement of 167
+  return 2      +167;
+}
---
+++ tests/programs/operators/test_order_of_operations2.c
@@ -0,0 +1,4 @@
+int main() {
+  // return the bitwise complement of 167
+  return 0 + (1 * (3 * ((5) - 2)));
+}
